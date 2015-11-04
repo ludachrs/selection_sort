1749e0033a4262e37e0d2cf9816743628d05494c
Chris Boccucci <ludachrs@mac.com>
2015-11-04 15:46:37 -0800

    Update Code

--- selection.py
+++ selection.py
@@ -1,11 +1,12 @@
 import random
 
 ran_num1 = [random.randint(0,10000) for r in xrange(100)] # v1
-# ran_num1 = [4,5,3,1,2]
+# ran_num1 = [4,5,3,9,2,7,1]
 # ran_num2 = [random.choice([i for i in xrange(10000)]) for r in xrange(10)] # v2 
 
 for i in range(0,len(ran_num1)):
 	min = i
+	max = i
 	for j in range(i+1,len(ran_num1)):
 		if ran_num1[j] < ran_num1[min]:
 			min = j
